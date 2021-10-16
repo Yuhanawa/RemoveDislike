638468e96a6e7031be12065556b71b33cb7bfb93
Yuhanawa <3261817910@qq.com>
2021-10-16 19:25:33 +0800

    rename

--- Cavitation/Src/Views/Models/CleanInfoTab.xaml.cs
+++ Cavitation/Src/Views/Models/CleanInfoTab.xaml.cs
@@ -4,9 +4,9 @@ using Cavitation.Views.Pages;
 
 namespace Cavitation.Views.Models
 {
-    public partial class InfoTab
+    public partial class CleanInfoTab
     {
-        public InfoTab()
+        public CleanInfoTab()
         {
             InitializeComponent();
         }
@@ -14,8 +14,8 @@ namespace Cavitation.Views.Models
         private void Play_OnClick(object sender, RoutedEventArgs e)
         {
             Cleaner.CleanerGroup[RuleName].Run();
-            ClearPage.Interface.State.Text = $"已清理： {Cleaner.AllCleanupSize} MB";
-            ClearPage.Interface.ReLoad();
+            CleanPage.Interface.State.Text = $"已清理： {Cleaner.AllCleanupSize} MB";
+            CleanPage.Interface.ReLoad();
         }
 
         private void Del_OnClick(object sender, RoutedEventArgs e)
@@ -23,26 +23,26 @@ namespace Cavitation.Views.Models
             if (MessageBox.Show("你确认要删除这个文件吗", "此功能不可逆！", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
                 Cleaner.CleanerGroup[RuleName].SelfDestruct();
 
-            ClearPage.Interface.ReLoad();
+            CleanPage.Interface.ReLoad();
         }
 
         private void More_OnClick(object sender, RoutedEventArgs e) =>
             // TODO add Secondary menu
-            ClearPage.Interface.ReLoad();
+            CleanPage.Interface.ReLoad();
 
         #region Property
 
         public static readonly DependencyProperty RuleNameProperty =
             DependencyProperty.Register("RuleName", typeof(string),
-                typeof(InfoTab), new PropertyMetadata("Unknown"));
+                typeof(CleanInfoTab), new PropertyMetadata("Unknown"));
 
         public static readonly DependencyProperty SourceProperty =
             DependencyProperty.Register("Source", typeof(string),
-                typeof(InfoTab), new PropertyMetadata("Unknown"));
+                typeof(CleanInfoTab), new PropertyMetadata("Unknown"));
 
         public static readonly DependencyProperty SizeProperty =
             DependencyProperty.Register("Size", typeof(double),
-                typeof(InfoTab), new PropertyMetadata(-1d));
+                typeof(CleanInfoTab), new PropertyMetadata(-1d));
 
         public string RuleName
         {
