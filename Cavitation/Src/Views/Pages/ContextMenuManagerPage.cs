b3f536447e931d100146a549675f38c13b4085a7
Yuhanawa <3261817910@qq.com>
2021-10-16 19:28:09 +0800

    new(semi finished product)

---
+++ Cavitation/Src/Views/Pages/ContextMenuManagerPage.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using Cavitation.Core.Utils;
+using Cavitation.Views.Models;
+using Microsoft.Win32;
+
+namespace Cavitation.Views.Pages
+{
+    public partial class ContextMenuManagerPage
+    {
+        public ContextMenuManagerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Temp_OnClick(object sender, RoutedEventArgs e)
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        private void General_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            RegistryKey general=  Core.Regedit.ContextMenu.GetGeneralMenu;            
+            RegistryKey shell =  Core.Regedit.ContextMenu.GeneralMenu.GetShell;
+            RegistryKey shellex =  Core.Regedit.ContextMenu.GeneralMenu.GetShellexcmh;
+            // RegistryKey _shellex =  Core.Regedit.ContextMenu.GeneralMenu.Get_shellexcmh;
+
+            foreach (string subKeyName in shell.GetSubKeyNames())
+                GeneralShellList.Children.Add(new ContextMenuInfoTab(true){Text = subKeyName});
+                        
+            foreach (string subKeyName in shellex.GetSubKeyNames())
+                GeneralShellExList.Children.Add(new ContextMenuInfoTab(true){Text = subKeyName});
+            
+        }
+    }
+}
