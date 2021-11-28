602e577a99cd409c7d6c25fc206f6e5c9bfc8c65
Yuhanawa <3261817910@qq.com>
2021-11-28 14:22:10 +0800

    load list

--- RemoveDislike/Src/Views/Models/ContextMenuInfoTab.xaml.cs
+++ RemoveDislike/Src/Views/Models/ContextMenuInfoTab.xaml.cs
@@ -1,30 +1,83 @@
 using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using RemoveDislike.Core.Utils;
 
 namespace RemoveDislike.Views.Models
 {
     public partial class ContextMenuInfoTab
     {
-        public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register("Text", typeof(string), typeof(ContextMenuInfoTab));
+        public static readonly DependencyProperty KeyNameProperty =
+            DependencyProperty.Register("KeyName", typeof(string), typeof(ContextMenuInfoTab));
 
         public static readonly DependencyProperty IconProperty =
             DependencyProperty.Register("Icon", typeof(ImageSource), typeof(ContextMenuInfoTab));
 
-        public bool TabVisibility;
 
-        public ContextMenuInfoTab(bool tabVisibility)
+        public ContextMenuInfoTab(RegistryKey key, string name)
         {
-            TabVisibility = tabVisibility;
             InitializeComponent();
+            DataContext = this;
+            KeyName = name;
+
+            var path = key.GetValue("Icon")?.ToString();
+            if (path == null)
+            {
+                // TODO (null value)
+            }
+            else if (path.EndsWith(".exe"))
+            {
+                var icon = System.Drawing.Icon.ExtractAssociatedIcon(path);
+
+                if (icon == null) return;
+
+                ImageSource img = Imaging.CreateBitmapSourceFromHBitmap(
+                    icon.ToBitmap().GetHbitmap(),
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+                Icon = img;
+            }
+            else if (path.Contains(".exe") && path.Contains(","))
+            {
+                var icon = System.Drawing.Icon.ExtractAssociatedIcon(path.Split(',')[0]);
+
+                if (icon == null) return;
+
+                ImageSource img = Imaging.CreateBitmapSourceFromHBitmap(
+                    icon.ToBitmap().GetHbitmap(),
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+                Icon = img;
+            }
+            else
+            {
+                Icon = new BitmapImage(new Uri(path));
+            }
         }
 
-        public string Text
+        public bool TabVisibility
         {
-            get => (string)GetValue(TextProperty);
-            set => SetValue(TextProperty, value);
+            get => !((RegistryKey)DataContext).GetValueNames().Contains("LegacyDisable");
+            set
+            {
+                if (value)
+                    ((RegistryKey)DataContext).DeleteValue("LegacyDisable");
+
+                ((RegistryKey)DataContext).SetValue("LegacyDisable", "");
+            }
+        }
+
+        public string KeyName
+        {
+            get => (string)GetValue(KeyNameProperty);
+            set => SetValue(KeyNameProperty, value);
         }
 
         public ImageSource Icon
@@ -40,13 +93,20 @@ namespace RemoveDislike.Views.Models
 
         #region Event
 
-        private void EIcon_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e) =>
-            throw new NotImplementedException();
+        private void EIcon_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // TODO
+        }
 
-        private void EText_OnPreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e) =>
-            throw new NotImplementedException();
+        private void EText_OnPreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // TODO
+        }
 
-        private void ESub_OnClick(object sender, RoutedEventArgs e) => throw new NotImplementedException();
+        private void ESub_OnClick(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
 
         private void EVisibility_OnClick(object sender, RoutedEventArgs e)
         {
@@ -56,32 +116,36 @@ namespace RemoveDislike.Views.Models
             EVisibility.Content = TabVisibility ? "" : "";
         }
 
-        private void EDel_OnClick(object sender, RoutedEventArgs e) => throw new NotImplementedException();
+        private void EDel_OnClick(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
 
-        private void EMore_OnClick(object sender, RoutedEventArgs e) => throw new NotImplementedException();
+        private void EMore_OnClick(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
 
         private void EIcon_OnPreviewDragEnter(object sender, DragEventArgs e)
         {
-            {
-                //仅支持文件的拖放
-                if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            //仅支持文件的拖放
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+
+            //获取拖拽的文件
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files == null || files.Length == 0) return;
+
+            foreach (string file in files)
+                try
+                {
+                    // TODO
+                }
+                catch (Exception ex)
+                {
+                    LogUtils.Err("Err", ex);
                     return;
-
-                //获取拖拽的文件
-                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
-
-                if (files == null || files.Length == 0)
-                    return;
-
-                foreach (string file in files)
-                    try
-                    {
-                    }
-                    catch
-                    {
-                        return;
-                    }
-            }
+                }
         }
 
         #endregion
--- RemoveDislike/Src/Views/Pages/ContextMenuManagerPage.cs
+++ RemoveDislike/Src/Views/Pages/ContextMenuManagerPage.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using Microsoft.Win32;
+using RemoveDislike.Core.Utils;
 using RemoveDislike.Views.Models;
 
 namespace RemoveDislike.Views.Pages
@@ -13,21 +16,92 @@ namespace RemoveDislike.Views.Pages
 
         private void Temp_OnClick(object sender, RoutedEventArgs e)
         {
-            // throw new System.NotImplementedException();
+            // TODO
         }
 
-        private void General_OnLoaded(object sender, RoutedEventArgs e)
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "IdentifierTypo")]
+        private void LoadGeneralList(object sender, EventArgs e)
         {
-            RegistryKey general = Core.Regedit.ContextMenu.GetGeneralMenu;
-            RegistryKey shell = Core.Regedit.ContextMenu.GeneralMenu.GetShell;
-            RegistryKey shellex = Core.Regedit.ContextMenu.GeneralMenu.GetShellexcmh;
-            // RegistryKey _shellex =  Core.Regedit.ContextMenu.GeneralMenu.Get_shellexcmh;
+            // GeneralMenu
+            RegistryKey shell = RegistryUtils.GeneralMenu.GetShell;
+            RegistryKey shellex = RegistryUtils.GeneralMenu.GetShellexcmh;
+            RegistryKey _shellex = RegistryUtils.GeneralMenu.Get_shellexcmh;
 
             foreach (string subKeyName in shell.GetSubKeyNames())
-                GeneralShellList.Children.Add(new ContextMenuInfoTab(true) { Text = subKeyName });
+                GeneralShellList.Children.Add(new ContextMenuInfoTab(shell.OpenSubKey(subKeyName), subKeyName));
 
             foreach (string subKeyName in shellex.GetSubKeyNames())
-                GeneralShellExList.Children.Add(new ContextMenuInfoTab(true) { Text = subKeyName });
+                GeneralShellExList.Children.Add(new ContextMenuInfoTab(shellex.OpenSubKey(subKeyName), subKeyName));
+
+            foreach (string subKeyName in _shellex.GetSubKeyNames())
+                General_ShellExList.Children.Add(new ContextMenuInfoTab(_shellex.OpenSubKey(subKeyName), subKeyName));
+        }
+
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "IdentifierTypo")]
+        private void LoadStarMenuList(object sender, EventArgs e)
+        {
+            // StarMenu
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "IdentifierTypo")]
+        private void LoadDesktopMenuList(object sender, EventArgs e)
+        {
+            // Directory\Background
+            RegistryKey shell = RegistryUtils.Directory.Background.GetShell;
+            RegistryKey shellex = RegistryUtils.Directory.Background.GetShellexcmh;
+            RegistryKey _shellex = RegistryUtils.Directory.Background.Get_shellexcmh;
+
+            foreach (string subKeyName in shell.GetSubKeyNames())
+                DesktopShellList.Children.Add(new ContextMenuInfoTab(shell.OpenSubKey(subKeyName), subKeyName));
+
+            foreach (string subKeyName in shellex.GetSubKeyNames())
+                DesktopShellExList.Children.Add(new ContextMenuInfoTab(shellex.OpenSubKey(subKeyName), subKeyName));
+
+            foreach (string subKeyName in _shellex.GetSubKeyNames())
+                Desktop_ShellExList.Children.Add(new ContextMenuInfoTab(_shellex.OpenSubKey(subKeyName), subKeyName));
+        }
+
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "IdentifierTypo")]
+        private void LoadFileMenuList(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "IdentifierTypo")]
+        private void LoadDirMenuList(object sender, EventArgs e)
+        {
+            // Directory
+            RegistryKey shell = RegistryUtils.Directory.GetShell;
+            RegistryKey shellex = RegistryUtils.Directory.GetShellexcmh;
+            RegistryKey _shellex = RegistryUtils.Directory.Get_shellexcmh;
+
+            foreach (string subKeyName in shell.GetSubKeyNames())
+                DirShellList.Children.Add(new ContextMenuInfoTab(shell.OpenSubKey(subKeyName), subKeyName));
+
+            foreach (string subKeyName in shellex.GetSubKeyNames())
+                DirShellExList.Children.Add(new ContextMenuInfoTab(shellex.OpenSubKey(subKeyName), subKeyName));
+
+            foreach (string subKeyName in _shellex.GetSubKeyNames())
+                Dir_ShellExList.Children.Add(new ContextMenuInfoTab(_shellex.OpenSubKey(subKeyName), subKeyName));
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        private void LoadIEMenuList(object sender, EventArgs e)
+        {
+            // HKEY_CURRENT_USER\SOFTWARE\Microsoft\Internet Explorer\MenuExt
+            RegistryKey menuExt =
+                Registry.CurrentUser.OpenSubKey(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Internet Explorer\MenuExt");
+
+            if (menuExt == null) return;
+            foreach (string subKeyName in menuExt.GetSubKeyNames())
+                IEList.Children.Add(new ContextMenuInfoTab(menuExt.OpenSubKey(subKeyName), subKeyName));
         }
     }
 }
