5ab6ee3ac6611e7bec857e68d2b9bbd00d020ae9
Yuhanawa <3261817910@qq.com>
2022-01-31 23:32:10 +0800

    :zap: :art:

--- RemoveDislike/Src/Core/Utils/DirectoryInfoUntils.cs
+++ RemoveDislike/Src/Core/Utils/DirectoryInfoUntils.cs
@@ -49,4 +49,23 @@ public static class DirectoryInfoUtils
     /// <param name="path">directory path</param>
     /// <returns>all path of files</returns>
     public static string[] TryGetAllFilePaths(string path) => new DirectoryInfo(path).TryGetAllFilePaths();
+    
+    /// <summary>
+    ///  Try to recursive traversal all path of files in the directory by directory path( ignore errors )
+    /// </summary>
+    /// <param name="path">directory path</param>
+    /// <param name="action"></param>
+    public static void TryGetAllFilePaths(string path, Action<string> action)
+    {
+        try
+        {
+            try { Directory.GetFiles(path).ToList().ForEach(action); }
+            catch (Exception e) { /* ignore errors */ }
+
+            foreach (string directory in Directory.GetDirectories(path))
+                try { TryGetAllFilePaths(directory, action); }
+                catch { /* ignore errors */ }
+        }
+        catch { /* ignore errors */ }
+    }
 }
