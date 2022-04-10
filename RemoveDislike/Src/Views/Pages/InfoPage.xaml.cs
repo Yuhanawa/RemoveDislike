7929d838d1126acf43063506258a3ab288fd7b7c
Yuhanawa <3261817910@qq.com>
2022-04-10 14:52:13 +0800

    add SummaryHardwarePanel

--- RemoveDislike/Src/Views/Pages/InfoPage.xaml.cs
+++ RemoveDislike/Src/Views/Pages/InfoPage.xaml.cs
@@ -1,8 +1,10 @@
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Management;
+using System.Windows;
 using System.Windows.Controls;
-using Microsoft.Win32;
+using Hardware.Info;
 using RemoveDislike.Views.Models;
 
 namespace RemoveDislike.Views.Pages;
@@ -14,20 +16,22 @@ public partial class InfoPage
     
 
     #endregion
+    public static readonly HardwareInfo info = new ();
     public InfoPage()
     {
+        info.RefreshAll();
         InitializeComponent();
         DataContext = this;
     }
     
     private static void PutInfoTab( Panel target,string title, string value) => 
-        target.Children.Add(new InfoTab(title, value));
+        target.Children.Add(new InfoTab(title, value){Margin = new Thickness(2)});
 
 
     private void SummarySoftwarePanel_OnInitialized(object sender, EventArgs e)
     {
         PutInfoTab((Panel)sender,"OS", Environment.OSVersion.ToString());
-        PutInfoTab((Panel)sender,"OS Root",  Environment.SystemDirectory);
+        PutInfoTab((Panel)sender,"OS Root",  EnvironmentUtils.Get("SystemRoot"));
         PutInfoTab((Panel)sender,"Architecture",  Environment.Is64BitOperatingSystem?"x64":"x86");
         PutInfoTab((Panel)sender,"Installation", GetWindowsInstallationDateTimeStr() );
         PutInfoTab((Panel)sender,"Product ID",  (from ManagementObject managementObject in new ManagementObjectSearcher("SELECT SerialNumber FROM Win32_OperatingSystem").Get() from PropertyData propertyData in managementObject.Properties where propertyData.Name == "SerialNumber" select (string)propertyData.Value).FirstOrDefault());
@@ -37,6 +41,19 @@ public partial class InfoPage
         PutInfoTab((Panel)sender,"UpTime",  GetSystemUpTimeInfo());
         PutInfoTab((Panel)sender,"TickCount", Environment.TickCount / 1000 + "s" );
         PutInfoTab((Panel)sender,"SystemPageSize", Environment.SystemPageSize / 1024 + "KB" );
+    }
+    
+    private void SummaryHardwarePanel_OnInitialized(object sender, EventArgs e)
+    {
+        
+        
+        PutInfoTab((Panel)sender,"CPU", info.CpuList.Aggregate("", (s, i) => s + $"{i.Name} "));
+        PutInfoTab((Panel)sender,"RAM", $"{info.MemoryStatus.TotalPhysical}b ({((double)info.MemoryStatus.TotalPhysical)/1024/1024/1024:F2}GB)");
+        PutInfoTab((Panel)sender,"Motherboard",  info.MotherboardList.Aggregate("", (s, i) => s+ $"{i.Product},{i.Manufacturer}"));
+        PutInfoTab((Panel)sender, "Graphics", info.VideoControllerList.Aggregate("", (s, i) => s + $"{i.Name} "));
+        PutInfoTab((Panel)sender,"Storage", info.DriveList.Select(i => (double)i.Size).Sum()/1024/1024/1024 + "GB");
+        PutInfoTab((Panel)sender,"Network", info.NetworkAdapterList.Aggregate("", (s, i) => s + $"{i.Name} "));
+        PutInfoTab((Panel)sender,"SoundCard",   info.SoundDeviceList.Aggregate("", (s, i) => s + $"{i.Name} "));
     }
 
     #region MyRegion
@@ -67,33 +84,11 @@ public partial class InfoPage
             return TimeSpan.Zero;
         }
     }
-    public static DateTime GetWindowsInstallationDateTime()
-    {
-        try
-        {
-            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
-
-                DateTime installDate = 
-                    DateTime.FromFileTimeUtc(
-                        Convert.ToInt64(
-                            key.GetValue("InstallDate").ToString()));
-     
-                return installDate;
-        }
-        catch
-        {
-            // ignored
-        }
-
-
-        return DateTime.MinValue;
-    }
-
     public static string GetWindowsInstallationDateTimeStr()
     {
         try
         {
-            DateTime date = GetWindowsInstallationDateTime();
+            DateTime date = Directory.GetCreationTime(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));;
             return $"{date.Year}-{date.Month}-{date.Day} {date.Hour}h:{date.Minute}m:{date.Second}s:{date.Millisecond}ms";
         }
         catch
@@ -104,4 +99,5 @@ public partial class InfoPage
     
     
     #endregion
+    
 }
