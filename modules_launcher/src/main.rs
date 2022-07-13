29292848c22d57d6f8f6379f47843148a93019be
Yuhanawa <3261817910@qq.com>
2022-07-13 19:43:59 +0800

    Add environment variable support

--- modules_launcher/src/main.rs
+++ modules_launcher/src/main.rs
@@ -1,12 +1,20 @@
 #![windows_subsystem = "windows"]
 
-
 use std::{env, fs, process};
 
+
 fn main() {
-    let s = fs::read_to_string(env::var("APPDATA").unwrap() + "/RemoveDislike/Modules/Launcher.config").unwrap();
-    s.split('\n').filter(|x| !x.starts_with("//") && !x.starts_with('#')).for_each(|x| {
-        let _ = process::Command::new(x.trim()).spawn();
-    });
+    fs::read_to_string(env::var("APPDATA").unwrap() + "/RemoveDislike/Modules/Launcher.config").unwrap().split('\n')
+        .filter(|x| !x.starts_with("//") && !x.starts_with('#')).for_each(|x| {
+        let mut x = x.trim().to_string();
+
+        if let Some(p) = regex::Regex::new("%.+%").unwrap().captures(&x.to_owned()) {
+            p.iter().filter(|p| p.is_some()).for_each(|m| {
+                x = x.replace(m.unwrap().as_str(), &env::var(m.unwrap().as_str().trim_matches('%')).unwrap());
+            })
+        }
+
+        let _ = process::Command::new(&x).spawn();
+    })
 }
 
