2dbd309b4d078858b7f3db2850f0eec40649c48c
Yuhanawa <3261817910@qq.com>
2022-07-14 16:12:27 +0800

    add ProcessUtils TryStart(string fileName, string arguments); TryStart(string fileName);

---
+++ RemoveDislike/Src/Core/Utils/ProcessUtils.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+
+namespace RemoveDislike.Core.Utils;
+
+public static class ProcessUtils
+{
+    public static void TryStart(string fileName, string arguments)
+    {
+        try
+        {
+            Process.Start(fileName, arguments);
+        }
+        catch (Exception ex)
+        {
+            Err($"Try to start process {fileName} with arguments {arguments} failed: {ex.Message}",ex);
+        }
+    }
+    public static void TryStart(string fileName)
+    {
+        try
+        {
+            Process.Start(fileName);
+        }
+        catch (Exception ex)
+        {
+            Err($"Try to start process {fileName} without arguments failed: {ex.Message}",ex);
+        }
+    }    
+}
