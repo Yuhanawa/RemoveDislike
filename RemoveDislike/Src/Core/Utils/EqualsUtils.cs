46d2c552f503388d5a5a22d30b48f206c89ba1ed
Yuhanawa <3261817910@qq.com>
2022-07-25 12:09:35 +0800

    add BoolAndBools(params bool?[] b) => b.Length<2||b.Any(x => x == null) ? null : b.All(x => x!.Value);

--- RemoveDislike/Src/Core/Utils/EqualsUtils.cs
+++ RemoveDislike/Src/Core/Utils/EqualsUtils.cs
@@ -1,4 +1,6 @@
 #nullable enable
+using System.Linq;
+
 namespace RemoveDislike.Core.Utils;
 
 public static class EqualsUtils
@@ -8,4 +10,6 @@ public static class EqualsUtils
 
     public static bool? ObjToStrEqRtBoolOrNull(this object? origin, object? target) => 
         origin?.ToString() == null ? null:target?.ToString() == null ? null : origin.ToString()!.StrEqRtBoolOrNull(target.ToString());
+
+    public static bool? BoolAndBools(params bool?[] b) => b.Length<2||b.Any(x => x == null) ? null : b.All(x => x!.Value);
 }
